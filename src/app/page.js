5d53b515dfd97167fa881a5694048a3395312eeb
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-15 02:28:14 +0600

    added banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      voyentra
+      <Banner></Banner>
     </div>
   );
 }
