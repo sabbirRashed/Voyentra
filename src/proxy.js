093160fab59731b91b50018f2002c1eeda9439ee
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-18 11:43:29 +0600

    added proxy for my-bookings, destination-details, add-booking

---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { headers } from "next/headers";
+import { auth } from "./lib/auth";
+import { NextResponse } from "next/server";
+
+
+export async function proxy(request) {
+    
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    if(session){
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/login', request.url));
+
+}
+
+
+export const config = {
+    matcher: ['/bookings', '/add-destination', '/destinations/:path+' ]
+}
